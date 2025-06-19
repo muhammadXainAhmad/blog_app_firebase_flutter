bc7a451b9651bcc6507f86b29fdac266236f98cf
muhammadXainAhmad <206599128+muhammadXainAhmad@users.noreply.github.com>
2025-06-20 01:49:14 +0500

    created constants.dart for ui

---
+++ lib/Utils/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+final bgClr = Colors.grey.shade200;
+final primaryClr = Colors.deepOrange;
+final whiteClr = Colors.white;
+final blackClr = Colors.black;
+
+final eBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.circular(20),
+  borderSide: BorderSide(color: Colors.grey.shade400, width: 1.5),
+);
+
+final fBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.circular(20),
+  borderSide: BorderSide(color: blackClr, width: 1.5),
+);
